79adab91bfc88fb82b6717807e7eb6cbcc6900d7
monkeypineapple <davidrosenholz@gmail.com>
2019-12-13 11:06:21 -0800

    :::sigh::::

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review
+    attr_accessor :viewer, :movie, :rating
 
 
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        @@all << self
+        movie.reviews << self
+        viewer.reviews << self
+    end
+
+    def viewer
+        viewer.self
+    end
+
+    def movie
+        movie.self
+    end
+
+    
 end
