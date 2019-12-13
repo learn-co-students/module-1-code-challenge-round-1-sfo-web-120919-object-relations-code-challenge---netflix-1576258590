24701be4e94ef8d6caec0b9fd8b94a8ae564092f
github username <github email address>
2019-12-13 11:07:56 -0800

    Attempted mock challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,16 @@
 class Review
-
-
+  attr_accessor :rating, :viewer, :movie
+ 
+  @@all = []
+  
+    def initialize(viewer, movie, rating)
+      @viewer = viewer
+      @movie = movie
+      @rating = rating
+      self.class.all << self
+    end
+  
+    def self.all
+      @@all
+    end  
 end
