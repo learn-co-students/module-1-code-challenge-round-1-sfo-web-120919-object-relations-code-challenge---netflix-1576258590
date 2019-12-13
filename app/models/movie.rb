bd834353ce31eb48ff4de3fad1d6865cf468e354
github username <github email address>
2019-12-13 11:06:25 -0800

    you need to learn for sure

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,26 @@
+# - `Movie#initialize(title)`
+#   - `Movie` is initialized with a title (string)
+#   - title **can be** changed after the `Movie` is initialized
+# - `Movie#title`
+#   - returns the `Movie`'s title
+# - `Movie.all`
+#   - returns an array of all the `Movie` instances that have been initialized
+
+#### Movie
+
+# - `Movie#reviews`
+#   - returns an array of all the `Review` instances for the `Movie`.
+# - `Movie#reviewers`
+#   - returns an array of all of the `Viewer` instances that reviewed the `Movie`.
+
+#### Movie
+
+# - `Movie#average_rating`
+#   - returns the average of all ratings for the `Movie` instance
+#   - to average ratings, add all ratings together and divide by the total number of ratings.
+# - `Movie.highest_rated`
+#   - returns the `Movie` instance with the highest average rating.
+
 class Movie
   attr_accessor :title
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,33 @@
+
+# - `Review#initialize(viewer, movie, rating)`
+#   - `Review` is initialized with a `Viewer` instance, a `Movie` instance, and a rating (number)
+# - `Review#rating`
+#   - returns the rating for the `Review` instance
+# - `Review.all`
+#   - returns an array of all initialized `Review` instances
+
+#### Review
+
+# - `Review#viewer`
+#   - returns the `Viewer` instance associated with the `Review` instance
+# - `Review#movie`
+#   - returns the `Movie` instance associated with the `Review` instance
 class Review
+
+    attr_accessor :viewer, :movie, :rating
+
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating.to_i
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 
 end
