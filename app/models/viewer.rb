072c9d4fd3898b093c8e6a4d9485f14d75dee31f
github username <github email address>
2019-12-13 11:08:21 -0800

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Viewer
   attr_accessor :username
 
@@ -5,11 +7,36 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def reviewed_movie?(movie)
+    if @username.movie == Review
+      return true
+    else
+      return false
+    #if reviewed movie return true
+    end
+  end
+
+
+  def review
+    reviewing = Review.all.select do |review|
+      review.viewer == self
+    end
+  end
+
+  def rate_movie(movie, rating)
+    if @username.movie == Review
+
+
+    # if the `Viewer` instance and the passed `Movie` instance are _not_ already associated, this method 
+    #should create a new `Review` instance
+    # if this `Viewer` has already reviewed this `Movie`, assigns the new rating to the existing `Review` instance
   end
   
 end
