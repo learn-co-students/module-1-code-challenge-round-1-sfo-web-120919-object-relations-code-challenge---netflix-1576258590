3206b05cbaa445aed72ad71e60dbb93dea7e6a91
github username <github email address>
2019-12-13 11:05:57 -0800

    Eduardo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,24 @@
 class Movie
-  attr_accessor :title
+  attr_accessor :title, :review, :reviewers
 
   @@all = []
+  @@review = []
+  @@reviewers = []
 
   def initialize(title)
     @title = title
     self.class.all << self
+    @@review << @movie.review
+    @@reviewers 
+
+
   end
 
   def self.all
     @@all
   end
+  
+  d
+
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,15 +1,26 @@
 class Viewer
-  attr_accessor :username
+  attr_accessor :username, :reviews, :movie
 
   @@all = []
+  @@reviews = []
+  @@movie = []
 
   def initialize(username)
     @username = username
     self.class.all << self
+    @@reviews << @username.viewer
+    @@movie << @username.movie
   end
 
   def self.all
-    @@all
+    self.all
   end
   
+  def self.review 
+    @@reviews 
+  end
+
+  def self.movie
+    @username + @@movie
+  end
 end
