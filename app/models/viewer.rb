d7cbd312e29e8dfd4509b34ad8f3fa154d52e833
brookebachman00 <yourpetlover4411@yahoo.com>
2019-12-13 11:03:51 -0800

    Pre-test

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,22 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+  def username
+    @username
+  end
+
+  def viewer#reviewed_movie?(movie)
+    if viewer.review
+      return true
+    else
+      return false
+    end
+  end
 end
