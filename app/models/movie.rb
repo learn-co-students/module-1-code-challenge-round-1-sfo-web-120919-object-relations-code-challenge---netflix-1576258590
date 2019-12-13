669bf4feef5d5e779b640279caf62b5fdae137f4
github username <github email address>
2019-12-13 11:08:21 -0800

    mock code challenge

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,5 +11,35 @@ class Movie
   def self.all
     @@all
   end
+#------------------------------------------------------
+
+
+  def reviews 
+    #returns an array of all the `Review` instances for the `Movie`.
+    self.all.review
+
+  end
+
+  def reviewers
+    #returns an array of all of the `Viewer` instances that reviewed the `Movie`.
+    self.all.viewer
+
+  end
+
+  def average_rating
+    # - returns the average of all ratings for the `Movie` instance
+    # - to average ratings, add all ratings together and divide by the total number of ratings.
+
+
+  end
+
+  def self.movie_highest_rated
+    #returns the `Movie` instance with the highest average rating.
+
+
+
+  end
+
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,50 @@
+#Hello lovely reviewers! 
+#I am a bit behind this week and am still learning one:many relationships, 
+#so all I really was able to do was set the structures up
+#I will catch up over the weekend- thanks!
+
+
 class Review
+    #class is empty upon start of challenge
+
+    attr_accessor :viewer, :movie, :rating
+
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+
+        @viewer = viewer
+        @movie = movie
+        @rating = rating 
+
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+      end
+
+
+    def rating
+    #returns the rating for the `Review` instance
+        @rating
+
+    end
+
+
+    def viewer
+    #returns the `Viewer` instance associated with the `Review` instance
+        self.viewer
+
+    end
+
+    def movie
+    # returns the `Movie` instance associated with the `Review` instance
+        self.movie
+        
+
+    end
 
 
 end
